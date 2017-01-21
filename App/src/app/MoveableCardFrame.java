65ffca8f0612619c5e77a278d512e51cab689b35
andrei lorin <andreilorin@gmail.com>
2017-01-21 14:17:46 +0000

    created MoveableCardFrame

---
+++ App/src/app/MoveableCardFrame.java
@@ -0,0 +1,67 @@
+package app;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+
+class MoveableCardFrame extends JButton {
+
+  private volatile int screenX = 0;
+  private volatile int screenY = 0;
+  private volatile int myX = 0;
+  private volatile int myY = 0;
+
+  public MoveableCardFrame() {
+    setBorder(new LineBorder(Color.BLUE, 3));
+    setBackground(Color.WHITE);
+    setBounds(0, 0, 110, 160);
+    setLocation(1000, 800);
+    setOpaque(false);
+
+    //add mouse listener
+    addMouseListener(new MouseListener() {
+
+      @Override
+      public void mouseClicked(MouseEvent e) { }
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+        screenX = e.getXOnScreen();
+        screenY = e.getYOnScreen();
+
+        myX = getX();
+        myY = getY();
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) { }
+
+      @Override
+      public void mouseEntered(MouseEvent e) { }
+
+      @Override
+      public void mouseExited(MouseEvent e) { }
+
+    });
+    
+    //add mouse motion listener
+    addMouseMotionListener(new MouseMotionListener() {
+
+      @Override
+      public void mouseDragged(MouseEvent e) {
+        int deltaX = e.getXOnScreen() - screenX;
+        int deltaY = e.getYOnScreen() - screenY;
+
+        setLocation(myX + deltaX, myY + deltaY);
+      }
+
+      @Override
+      public void mouseMoved(MouseEvent e) { }
+
+    });
+  }
+
+}
